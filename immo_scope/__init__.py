62e780c46e9c9dddc3661e0eedf6c4458c2e36d0
MAMY Rodrigue <rodrigue.mamy@etu.univ-amu.fr>
2025-11-21 04:26:18 +0100

    Configuration du package Python immo_scope
    - Export des classes DataLoader et PlotVisualizer
    - Métadonnées du package (version, auteur, description)
    - Structure modulaire professionnelle

--- immo_scope/__init__.py
+++ immo_scope/__init__.py
@@ -1,5 +1,12 @@
+"""
+Package immo_scope - Analyse des données immobilières DVF
+"""
+
 from .data_loader import DataLoader
 from .visualizer import PlotVisualizer
 
 __version__ = "1.0.0"
 __author__ = "Rodrigue Mamy, Benaissa Hadjer, Léa Benameur"
+__description__ = "Package pour l'analyse des données DVF avec dashboard interactif"
+
+__all__ = ["DataLoader", "PlotVisualizer"]
