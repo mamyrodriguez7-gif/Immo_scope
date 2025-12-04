055dfee988690909ef38c441253c1b7ce9588a17
benaissahadjer <benaissahadjer143@gmail.com>
2025-12-04 21:56:03 +0100

    Update __init__.py

--- immo_scope/__init__.py
+++ immo_scope/__init__.py
@@ -2,11 +2,12 @@
 Package immo_scope - Analyse des données immobilières DVF
 """
 
-from .data_loader import DataLoader
-from .visualizer import PlotVisualizer
+from .visualizer import Visualizer
+from .map_visualizer import MapVisualizer
 
 __version__ = "1.0.0"
 __author__ = "Rodrigue Mamy, Benaissa Hadjer, Léa Benameur"
 __description__ = "Package pour l'analyse des données DVF avec dashboard interactif"
 
-__all__ = ["DataLoader", "PlotVisualizer"]
+__all__ = ["Visualizer", "MapVisualizer"]
+
